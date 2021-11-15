97dfd3f96f87eb1d17144639c5027a75ff3afe8e
VarenM <93215720+VarenM@users.noreply.github.com>
2021-11-14 21:29:27 -0500

    Update CSG.h

--- CSG.h
+++ CSG.h
@@ -2,23 +2,35 @@
 #define _CSG_h
 
 #include <stdint.h>
-
-typedef char ETYPE[32];
+#include "CDH.h"
+#include "CR.h"
 
 typedef struct CSG* CSGTUPLE;
 
-typedef CSGTUPLE IDTABLE[1009];
-typedef CSGTUPLE COURSETABLE[1009];
-typedef CSGTUPLE GRADETABLE[1009];
+typedef CSGTUPLE CSGTABLE[1009];
 
-extern CSGTUPLE new_CSGTUPLE(char ID[5], char course[5], char grade[2]);
+extern CSGTUPLE new_CSGTUPLE(char course[5], char ID[5], char grade[2]);
 
-extern void CSGInsert(CSGTUPLE tuple, IDTABLE IDTable, COURSETABLE courseTable, GRADETABLE gradeTable);
+extern void CSGInsert(CSGTUPLE tuple, CSGTABLE table);
 
-extern void testInsert(CSGTUPLE tuple, IDTABLE IDTable);
+extern void CSGDelete(CSGTUPLE tuple, CSGTABLE table);
 
-extern void printInfo(IDTABLE table);
+extern void CSGdelete(CSGTUPLE tuple, CSGTABLE table, int hash);
 
-extern void printInfo2(IDTABLE table);
+extern void CSGLookup(CSGTUPLE tuple, CSGTABLE table, CSGTUPLE set[1009]);
+
+extern void CSGlookup(CSGTUPLE tuple, CSGTABLE table, int hash, CSGTUPLE set[1009]);
+
+extern void printCSGInfo(CSGTUPLE tuple);
+
+extern void printCSGTable(CSGTABLE table);
+
+extern void printCSGLookup(CSGTUPLE set[1009]);
+
+extern int CSGh(char x[32]);
+
+extern char* gradeFinder(CSGTABLE table, char name[32], char course[5]);
+
+extern char* CSGcourseFinder(CSGTABLE table, CDHTABLE CDHtable, CRTABLE CRtable, char hour[4], char day[2], char id[5]);
 
 #endif
